833d55b49ffc700ff0f843daea0720eb2fc442b4
sagarpatel2570 <sagarpatel25700@gmail.com>
2020-04-03 02:36:47 +0530

    added bullet effect and camera shake

--- 1943 Prototype/Assets/Scripts/Character/DamageOnTouch.cs
+++ 1943 Prototype/Assets/Scripts/Character/DamageOnTouch.cs
@@ -8,6 +8,7 @@ public class DamageOnTouch : MonoBehaviour
     public LayerMask targetLayerMask;
     public bool destroyOnTrigger;
     public int damage;
+    public GameObject damageEffect;
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
@@ -15,10 +16,15 @@ public class DamageOnTouch : MonoBehaviour
         {
             return;
         }
+        CameraShake.Shake();
 
         ITakeDamage obj = collider.GetComponentInParent<ITakeDamage>();
         if(obj != null)
         {
+            if (obj != null)
+            {
+                SimplePool.Spawn(damageEffect.gameObject, transform.position, Quaternion.identity);
+            }
             obj.TakeDamage(damage);
         }
 
